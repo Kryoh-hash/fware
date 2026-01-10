e031e890a16c15f1304b2ca5c4398723ffcae39d
Kryoh-hash <60992380+Kryoh-hash@users.noreply.github.com>
2026-01-10 01:55:56 +0000

    Update NodeDB.cpp

--- src/mesh/NodeDB.cpp
+++ src/mesh/NodeDB.cpp
@@ -577,7 +577,11 @@ void NodeDB::installDefaultConfig(bool preserveKey = false)
 #else
     config.lora.modem_preset = meshtastic_Config_LoRaConfig_ModemPreset_LONG_FAST;
 #endif
+#ifdef USERPREFS_LORACONFIG_HOP_LIMIT
+    config.lora.hop_limit = USERPREFS_LORACONFIG_HOP_LIMIT;
+#else
     config.lora.hop_limit = HOP_RELIABLE;
+#endif
 #ifdef USERPREFS_CONFIG_LORA_IGNORE_MQTT
     config.lora.ignore_mqtt = USERPREFS_CONFIG_LORA_IGNORE_MQTT;
 #else
